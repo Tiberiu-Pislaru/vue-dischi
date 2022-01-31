c9792e419ad6ffe5365de8f368efb93e1beb58c0
Tiberiu-Pislaru <tiberiupislaru03@gmail.com>
2022-01-31 17:24:26 +0100

    Aggiuno Loader

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <container-logo/>
-    <container-disks :disks='listDisks'/>
+    <container-logo :check='listDisks'/>
+    <container-disks :disks='listDisks' />
+    <loader-box v-if="this.listDisks.length===0"/>
   </div>
 </template>
 
@@ -9,6 +10,7 @@
 import axios from 'axios';
 import ContainerDisks from './components/ContainerDisks.vue'
 import ContainerLogo from './components/ContainerLogo.vue'
+import LoaderBox from './components/LoaderBox.vue'
 
 export default {
   name: 'App',
@@ -20,10 +22,11 @@ export default {
   components: {
     ContainerDisks,
     ContainerLogo,
+    LoaderBox
   },
   mounted() {
     axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((result)=>{
-      
+      console.log(result.data.response.length)
       this.listDisks=result.data.response;
     })
   }
